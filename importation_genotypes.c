a6fbb55137b7dfd29de81c9629ed800d7af757ae
leolinux <leolinux@leolinux-VirtualBox.(none)>
2013-12-08 21:53:14 +0100

    Ajout de commentaires

--- importation_genotypes.c
+++ importation_genotypes.c
@@ -7,21 +7,23 @@
 /***********************************************************************
  * importation_genotypes
  **********************************************************************/
-//PRE CONDITIONS
-//POST CONDITIONS
-
+/*PRE CONDITIONS
+Les génotypes se trouvent dans le fichier créé par le programme "generateur_genotypes"
+POST CONDITIONS
+Les génotypes sont importés depuis le fichier dans un tableau alloué dynamiquement nommé "tab_individus"
+*/
 //Importation des génotypes des individus depuis le fichier vers un tableau dynamique 
 void importation_genotypes(char* genotype_file, T_info* pvar)
 {
 	int i;
-	int taille; 
-	int nb_ind; 
+	int taille; // Taille des génotypes
+	int nb_ind; // Nombre d'individus dans le fichier
 	FILE* file = NULL;
 	
-	file = init_file_ptr(genotype_file, "r");
-	taille = nb_char(file);
+	file = init_file_ptr(genotype_file, "r"); // Ouverture du fichier en mode écriture
+	taille = nb_char(file); // La taille des génotypes vaut le nombre de caractère de la première ligne du fichier
 	rewind(file); // Retour au début du fichier
-	nb_ind = nb_ligne(file);
+	nb_ind = nb_ligne(file); // Le nombre d'individus vaut le nombre de lignes dans le fichier
 	rewind(file);
 	
 	printf("\nTaille des génotypes = %d, Nombre d'individus = %d\n\n", taille, nb_ind);
@@ -31,13 +33,13 @@ void importation_genotypes(char* genotype_file, T_info* pvar)
 	
 	for (i = 0; i < nb_ind; i++) // Pour chaque individu
 	{
-		pvar->tab_individus[i].sequence = malloc_char_string(taille + 1);
+		pvar->tab_individus[i].sequence = malloc_char_string(taille + 1); // Allocation mémoire pour la séquence dans le tableau d'individus
 		fgets(pvar->tab_individus[i].sequence, taille + 2, file); // Remplissage du tableau par fgets
 		pvar->tab_individus[i].sequence[taille] = '\0'; // Remplacement de \n par \0
-		///printf("Individu #%d\t Sequence: %s\t\n", i, pvar->tab_individus[i].sequence);
+		//printf("Individu #%d\t Sequence: %s\t\n", i, pvar->tab_individus[i].sequence);
 	}
 	
-	fclose(file);
+	fclose(file); // Fermeture du fichier
 	pvar->taille = taille;
 	pvar->nb_ind = nb_ind;
 	
@@ -47,24 +49,24 @@ void importation_genotypes(char* genotype_file, T_info* pvar)
 /***********************************************************************
  * nb_lignes
  **********************************************************************/
-// Compte le nombre de lignes du fichier
+// Comptage du nombre de lignes dans le fichier
 int nb_ligne (FILE *fp)
 {
 	int n = 0, c;
-	while ((c = fgetc(fp)) != EOF)
-		if (c == '\n')
+	while ((c = fgetc(fp)) != EOF) // Tant que la fin du fichier n'est pas atteinte
+		if (c == '\n') // Si on recontre un retour à la ligne, on incrémente le nombre de lignes comptées
 			n++;
-	return n;
+	return n; // On retourne le nombre de lignes total
 }
 
 /***********************************************************************
  * nb_char_per_line
  **********************************************************************/
-// Compte le nombre de char de la première ligne du fichier
+// Comptage du nombre de caractère de la première ligne du fichier
 int nb_char (FILE *fp)
 {
 	int n = 0, c;
-	while ((c = fgetc(fp)) != '\n')
-		n++;
-	return n;
+	while ((c = fgetc(fp)) != '\n') // Tant que la fin de la première ligne du fichier n'est pas atteinte
+		n++; // On incrémente le nombre de caractère
+	return n; // On retourne le nombre de caractère total de la première ligne
 }
